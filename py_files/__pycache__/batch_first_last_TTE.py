7a681d8ecdeec3326e80113ec3542e700b8bf7b3
rhulseyv <rhulseyv@uoregon.edu>
2026-02-23 11:33:38 -0800

    Add .gitignore (ignore Python cache files)

--- py_files/__pycache__/batch_first_last_TTE.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-#  Call the organizer function here
-
-
-# graph number of songs for each hour (of each day), overlay line plots from each day
-
-
-# graph number of songs by day
